d03d8b72693cf110866ca44eb66aa07b3836c203
RemInstant <pboykov04@mail.ru>
2025-04-23 20:18:13 +0300

    initial setup

---
+++ client/src/main/java/org/reminstant/cryptomessengerclient/CryptoMessengerClientApplication.java
@@ -0,0 +1,13 @@
+package org.reminstant.cryptomessengerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMessengerClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CryptoMessengerClientApplication.class, args);
+  }
+
+}
---
+++ server/src/test/java/org/reminstant/cryptomessenger/CryptoMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reminstant.cryptomessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMessengerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
